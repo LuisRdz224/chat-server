a858ce4d291385b0f4c9e78b7564f67a46970983
LuisRdz224 <luis.rodriguez@fyware.com>
2024-06-15 15:10:18 -0600

    ADD: Chats creation endpoint working

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
 import userRoutes from './users';
+import userChatsRoutes from './chats';
 
 const router = Router();
 router.use('/api/users', userRoutes)
+router.use('/api/chats', userChatsRoutes)
 
 export default router;
